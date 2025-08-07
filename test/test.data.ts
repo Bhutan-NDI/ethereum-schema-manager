e045b239d441502eda78ab7e84f30324728ea59d
kinxa0 <kinzangdorji@dhi.bt>
2025-08-07 15:22:50 +0600

    Update test-data

--- test/test.data.ts
+++ test/test.data.ts
@@ -3,17 +3,15 @@ import { SchemaManagerConfig } from '../src/types/EthereumSchemaManager.types'
 export const ethSchemaManagerClientConfig: SchemaManagerConfig = {
   privateKey:
     '3f6254328fa58202094c954d89964119830f85e2f4bfdbabb1d8bcfc008d2fdd',
-  rpcUrl:
-    'https://eth-sepolia.g.alchemy.com/v2/XUo--fMnn250sIOxldOhx1J9-rput18B',
-  contractAddress: '0x0e54FD0c32271a4512972Dc9d46cAb74272ae1bb',
+  rpcUrl: 'https://eth-sepolia.g.alchemy.com/v2/API-KEY',
+  contractAddress: '0xBA3971c8648e446BD506d95318D391cBaa9B1c64',
 }
 
 export const ethSchemaManagerAdminConfig: SchemaManagerConfig = {
   privateKey:
     'c0fe3af6dc7188d1badd556303c8e3f1d60c19df3d84a380a16335a2d9a9c65e',
-  rpcUrl:
-    'https://eth-sepolia.g.alchemy.com/v2/XUo--fMnn250sIOxldOhx1J9-rput18B',
-  contractAddress: '0x0e54FD0c32271a4512972Dc9d46cAb74272ae1bb',
+  rpcUrl: 'https://eth-sepolia.g.alchemy.com/v2/API-KEY',
+  contractAddress: '0xBA3971c8648e446BD506d95318D391cBaa9B1c64',
 }
 
 export const testSchemaSample = {
