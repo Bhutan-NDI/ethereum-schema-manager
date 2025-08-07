2b8dc075f0696343d23db037260f0d763fda7de4
kinxa0 <kinzangdorji@dhi.bt>
2025-08-07 12:03:54 +0600

    Export types and errors

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export * from './EthereumSchemaManager'
+export { EthereumSchemaManager } from './EthereumSchemaManager'
+export type { SchemaManagerConfig } from './types/EthereumSchemaManager.types'
+export {
+  SchemaRegistryError,
+  ContractError,
+  NetworkError,
+  ValidationError,
+} from './types/EthereumSchemaManager.types'
